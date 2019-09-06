080301f6f14044337f9f67fe773571bc813f9650
AULA F.DE ING.DE SISTEMAS E INF <AULAFISI@UNMSM.pe>
2019-09-06 12:25:26 -0500

    first commit

---
+++ Untitled1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int a,b,suma;
+    printf("Ingrese 2 numeros enteros: \n");
+    scanf("%i",&a);
+    scanf("%i",&b);
+    suma=a+b;
+    printf("La suma de los numeros es: %i \n",suma);
+system("pause");
+return(0);
+}
